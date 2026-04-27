83ad0951ce03581ac73d8a6fb4836c5b88953fc3
prayagacodes <prayaga.ganga@gmail.com>
2026-04-27 17:05:16 +0530

    Created calculator program using switch case

---
+++ calculator.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char op;
+    double num1, num2;
+
+    cout << "Simple Calculator\n";
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch(op) {
+        case '+':
+            cout << "Result = " << num1 + num2;
+            break;
+
+        case '-':
+            cout << "Result = " << num1 - num2;
+            break;
+
+        case '*':
+            cout << "Result = " << num1 * num2;
+            break;
+
+        case '/':
+            if(num2 != 0)
+                cout << "Result = " << num1 / num2;
+            else
+                cout << "Error! Division by zero.";
+            break;
+
+        default:
+            cout << "Invalid operator!";
+    }
+
+    return 0;
+}
